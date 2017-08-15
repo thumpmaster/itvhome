3c78fae7589a59f87e40fae6e82c314c952f90f3
Prasad <prasad@salesgossip.co.uk>
2017-08-15 09:23:11 +0100

    Update composer with larcast integrated pacakage and add functional tests

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,18 +1,93 @@
 <?php
 
-namespace Tests\Feature;
+use Laracasts\Integrated\Extensions\Laravel as IntegrationTest;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class HomePageTest extends TestCase
+class HomePageTest extends IntegrationTest
 {
 
+    /** @test */
     public function testHomePage()
     {
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $this->visit('/');
     }
+
+    /** @test */
+    public function testHomePageWithTextExists()
+    {
+        $this->visit('/')
+            ->see('itv Homepage');
+    }
+
+
+    /** @test */
+    public function testNumberOfHeroBannerOnHomePage()
+    {
+        $defaultNumberOfHeroBanners = 3;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('#banner', $defaultNumberOfHeroBanners);
+    }
+
+
+    /** @test */
+    public function testNumberOfSmallBannersOnHomePage()
+    {
+        $defaultNumberOfSmallBanners = 18;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('image featured', $defaultNumberOfSmallBanners);
+    }
+
+
+    /** @test */
+    public function testTitlesOnHeroBanners()
+    {
+        $defaultNumberOfHeroBanners = 3;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('#banner header h2', $defaultNumberOfHeroBanners);
+    }
+
+    /** @test */
+    public function testDescriptionOnHeroBanners()
+    {
+        $defaultNumberOfHeroBanners = 3;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('#banner header p', $defaultNumberOfHeroBanners);
+    }
+
+
+    /** @test */
+    public function testTitlesOnSmallBanners()
+    {
+        $defaultNumberOfSmallBanners = 18;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('.box header h3', $defaultNumberOfSmallBanners);
+    }
+
+
+    /** @test */
+    public function testDescriptionOnSmallBanners()
+    {
+        $defaultNumberOfSmallBanners = 18;
+        $this->visit('/')
+            ->see('itv Homepage')
+            ->countElements('.box header p', $defaultNumberOfSmallBanners);
+    }
+
+
+    /**
+     * Verify the number of dom elements
+     * @param  string   $selector the dom selector (jquery style)
+     * @param  int      $number   how many elements should be present in the dom
+     * @return $this
+     */
+    public function countElements($selector, $number)
+    {
+        $this->assertCount($number, $this->crawler->filter($selector));
+        return $this;
+    }
+
 }
